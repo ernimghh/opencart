aa93fd8e6263c0c62f6197e8fe401c2936c5b2b9
ernimghh <sergvolohan@tut.by>
2025-11-15 23:10:17 +0300

    Initial commit

---
+++ adminopencart/language/ru-ru/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<HR><a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Все права защищены.<br> <BR> <a href="http://opencart.ru" target="_blank">OpenCart.ru</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="https://www.opencart.ru/support/" target="_blank">Техническая поддержка</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="https://www.opencart.ru/katalog/" target="_blank">Магазин дополнений</a>';
+$_['text_version'] = 'Версия %s';
